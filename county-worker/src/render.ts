fed1b3c3863687d5bd10d15be7fd4d980cf0cb91
CraftedSpot <photog12@gmail.com>
2026-02-13 10:43:20 -0600

    c

--- county-worker/src/render.ts
+++ county-worker/src/render.ts
@@ -159,6 +159,7 @@ const HEADER = `
             <nav class="nav-links">
                 <a href="/features">Features</a>
                 <a href="/pricing">Pricing</a>
+                <a href="/insights">Insights</a>
                 <a href="/about">About</a>
                 <a href="/counties/">Counties</a>
                 <a href="https://portal.mymineralwatch.com" class="btn-login">Sign In</a>
