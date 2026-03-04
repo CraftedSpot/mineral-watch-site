400a44a449a05ac7dd3c2d54ecaacce2fa7e3521
CraftedSpot <photog12@gmail.com>
2026-03-03 18:11:28 -0600

    c

--- portal-worker/src/templates/title-builder.ts
+++ portal-worker/src/templates/title-builder.ts
@@ -13,6 +13,13 @@ import sharedDocHelpers from './scripts/shared-doc-helpers.txt';
 import sharedDocDetail from './scripts/shared-document-detail.txt';
 import sharedOpenDocDetail from './scripts/shared-open-doc-detail.txt';
 import sharedPropertyDetail from './scripts/shared-property-detail.txt';
+import sharedWellDetail from './scripts/shared-well-detail.txt';
+import sharedOccHelpers from './scripts/shared-occ-helpers.txt';
+import sharedOtcProduction from './scripts/shared-otc-production.txt';
+import sharedOtcCss from './styles/shared-otc-production.txt';
+import sharedOccFilings from './scripts/shared-occ-filings.txt';
+import sharedCompletionReports from './scripts/shared-completion-reports.txt';
+import sharedDrillingPermits from './scripts/shared-drilling-permits.txt';
 
 // Title page JS and CSS
 import titlePageJs from './scripts/title-page.txt';
@@ -28,6 +35,13 @@ html = html.replace('/* __SHARED_DOC_HELPERS__ */\n', () => sharedDocHelpers);
 html = html.replace('/* __SHARED_DOCUMENT_DETAIL__ */\n', () => sharedDocDetail);
 html = html.replace('/* __SHARED_OPEN_DOC_DETAIL__ */\n', () => sharedOpenDocDetail);
 html = html.replace('/* __SHARED_PROPERTY_DETAIL__ */\n', () => sharedPropertyDetail);
+html = html.replace('/* __SHARED_WELL_DETAIL__ */\n', () => sharedWellDetail);
+html = html.replace('/* __SHARED_OCC_HELPERS__ */\n', () => sharedOccHelpers);
+html = html.replace('/* __SHARED_OTC_PRODUCTION__ */\n', () => sharedOtcProduction);
+html = html.replace('/* __SHARED_OTC_CSS__ */\n', () => sharedOtcCss);
+html = html.replace('/* __SHARED_OCC_FILINGS__ */\n', () => sharedOccFilings);
+html = html.replace('/* __SHARED_COMPLETION_REPORTS__ */\n', () => sharedCompletionReports);
+html = html.replace('/* __SHARED_DRILLING_PERMITS__ */\n', () => sharedDrillingPermits);
 html = html.replace('/* __TITLE_PAGE_JS__ */\n', () => titlePageJs);
 html = html.replace('/* __TITLE_PAGE_CSS__ */\n', () => titlePageCss);
 
